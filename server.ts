cea9107d9bbdbc99b3f589ad6c9c8774eadbd6ce
amr8644 <amr8644@gmail.com>
2022-05-04 15:34:19 +0300

    updated package.json

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+const path = require("path");
 const app = express();
 const dotenv = require("dotenv").config();
 const connectDB = require("./connection/db.ts");
@@ -12,6 +13,16 @@ connectDB();
 app.use(express.json({ limit: "50mb" }));
 
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
+
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static(path.join(__dirname, "/client/build")));
+
+   app.get("*", (req, res) =>
+      res.sendFile(
+         path.resolve(__dirname, "/", "client", "build", "index.html")
+      )
+   );
+}
 
 app.use("/api/users", userRoute);
 app.use("/api/books", bookRoute);
